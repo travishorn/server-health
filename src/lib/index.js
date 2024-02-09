8d9ee879fbb071c2498f95ff5cdc7b5888ec8de9
Travis Horn <travis@travishorn.com>
2024-02-09 09:12:02 -0600

    Reboot required

--- src/lib/index.js
+++ src/lib/index.js
@@ -33,7 +33,7 @@ export async function serverCommand(command) {
 				});
 
 				stream.stderr.on('data', (/** @type {string} */ data) => {
-					reject(data);
+					reject(data.toString());
 				});
 			});
 		});
@@ -52,15 +52,24 @@ export async function getSystemUpSince() {
 }
 
 export async function getLoadAverages() {
-	const uptimeOutput = await serverCommand('uptime');
+	const stdout = await serverCommand('uptime');
 
 	const uptimeRegex =
 		/up\s+(.*?),\s+([\s\S]+?)\s+user.*load average:\s+([\d.]+),\s+([\d.]+),\s+([\d.]+)/;
-	const matches = uptimeOutput.match(uptimeRegex);
+	const matches = stdout.match(uptimeRegex);
 
 	return {
 		'1min': parseFloat(matches[3]),
 		'5min': parseFloat(matches[4]),
 		'15min': parseFloat(matches[5])
 	};
+}
+
+export async function getRebootRequired() {
+	try {
+		const stdout = await serverCommand('ls /var/run/reboot-required');
+		return stdout === '/var/run/reboot-required\n';
+	} catch (err) {
+		return err === "ls: cannot access '/var/run/reboot-required': No such file or directory";
+	}
 }
--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,12 +1,14 @@
-import { getSystemUpSince, getLoadAverages } from '../lib/index.js';
+import { getSystemUpSince, getLoadAverages, getRebootRequired } from '../lib/index.js';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
 	const systemUpSince = await getSystemUpSince();
 	const loadAverages = await getLoadAverages();
+	const rebootRequired = await getRebootRequired();
 
 	return {
 		systemUpSince,
-		loadAverages
+		loadAverages,
+		rebootRequired
 	};
 }
